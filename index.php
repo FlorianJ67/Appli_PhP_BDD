136c8def22ff8bf62b2401d5f3ada4559bc7d2c2
Florian JACOB <florian.jacob96@gmail.com>
2023-01-12 17:00:02 +0100

    prepare the css

--- index.php
+++ index.php
@@ -3,9 +3,45 @@ session_start();
 include "functions.php";
 require_once('db-functions.php');
 
-
 $store = findAll();
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device_width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<title>Magasin Retro</title>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<a href="index.php">
+				<i class="fa fa-home"></i>
+			</a>
+			<ul>
+				<li>
+					<a href="index.php">INDEX</a>
+				</li>
+				<li>
+					<a href="recap.php">RÉCAP</a>
+				</li>
+			</ul>
+			<a href="recap.php" <?php if (nbProduits() == null || nbProduits() == 0) {
+									echo "style= 'display: none'";
+								} ?>>
+				<i class="fa fa-shopping-cart"></i>
+				<p id="shoppingCart"><?= nbProduits() ?></p>
+			</a>
+		</nav>
+	</header>
+</body>
+<?php
 // On affiche chaques produits un a un
 
 foreach ($store as $product) {
--- product.php
+++ product.php
@@ -1,20 +1,61 @@
 <?php
-
+session_start();
+include "functions.php";
 require_once('db-functions.php');
 
 $product = findOneById($_GET['id']);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div>
-	<a href="index.php">Retour</a>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device_width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-	<h1><?= $product['name'] ?></h1>
+	<title>Ajout produit</title>
+</head>
 
-	<p class='description'><?= $product['name'] ?></p>
+<body>
+	<header>
+		<nav>
+			<a href="index.php">
+				<i class="fa fa-home"></i>
+			</a>
+			<ul>
+				<li>
+					<a href="index.php">INDEX</a>
+				</li>
+				<li>
+					<a href="recap.php">RÉCAP</a>
+				</li>
+			</ul>
+			<a href="recap.php" <?php if (nbProduits() == null || nbProduits() == 0) {
+									echo "style= 'display: none'";
+								} ?>>
+				<i class="fa fa-shopping-cart"></i>
+				<p id="shoppingCart"><?= nbProduits() ?></p>
+			</a>
+		</nav>
+	</header>
+	<main>
+		<div id="container">
+			<article id="productFocus">
+				<div id="productInfo">
+					<a href="index.php">Retour</a>
 
-	<p><?= $product['price'] ?></p>
+					<h1><?= $product['name'] ?></h1>
 
-	<a href="traitement.php?action=addToCart&id=<?= $_GET['id'] ?>">Ajouter un produit</a>
+					<p class='description'><?= $product['name'] ?></p>
 
-</div>
+					<p id="productPrice"><?= $product['price'] ?></p>
+
+					<a href="traitement.php?action=addToCart&id=<?= $_GET['id'] ?>">Ajouter un produit</a>
+				</div>
+
+			</article>
+		</div>
+	</main>
+</body>
